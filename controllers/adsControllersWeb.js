472b69afb3839de89d94348bd74873fa7b036d58
Saralopezlovon <lopezlovonsara@gmail.com>
2021-11-24 07:03:15 +0100

    Add Home

--- controllers/adsControllersWeb.js
+++ controllers/adsControllersWeb.js
@@ -4,7 +4,16 @@ const adsWeb = {
 
     getHome: async(req, res)=>{
         try{
-            res.status(200).render('home', {name: "Ricardo"})
+            res.status(200).render('home',
+             {urlImage: "https://www.eluniversal.com.mx/sites/default/files/2019/07/12/amazon.jpg",
+             title: "FullStack Developer",
+             nameCompany: "Amazon",
+             location: "España-Madrid",
+             salary: "1800",
+             description: "Importante empresa del sector ubicada en Madrid se encuentra en la búsqueda de un desarrollador Fullstack",
+             date:"24/11/2021",
+             link:"https://acortar.link/ccGwMk"
+                })
 
         }catch(err){
             res.status(400).json({
