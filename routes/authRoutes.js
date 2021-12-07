b7f54f0ba7a68614f4dfecbc9739fd6942580bf0
Ricardo <bescir@gmail.com>
2021-12-07 13:46:02 +0100

    admin permissions done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -43,16 +43,16 @@ router.get('/callback', function (req, res, next) {
 router.get('/logout', (req, res) => {
     req.logout();
 
-    let returnTo = req.protocol + '://' + req.hostname;
-    const port = req.connection.localPort;
+    var returnTo = req.protocol + '://' + req.hostname;
+    var port = req.connection.localPort;
     if (port !== undefined && port !== 80 && port !== 443) {
         returnTo += ':' + port;
     }
 
-    const logoutURL = new url.URL(
+    var logoutURL = new url.URL(
         util.format('https://%s/v2/logout', process.env.AUTH0_DOMAIN)
     );
-    const searchString = querystring.stringify({
+    var searchString = querystring.stringify({
         client_id: process.env.AUTH0_CLIENT_ID,
         returnTo: returnTo,
     });
