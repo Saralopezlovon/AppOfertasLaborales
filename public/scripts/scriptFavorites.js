d6458ce90d2c88eb3a54f678ab2c36394481c186
Sara <lopezlovonsara@gmail.com>
2021-12-07 16:16:50 +0100

    Ad object built

--- public/scripts/scriptFavorites.js
+++ public/scripts/scriptFavorites.js
@@ -1,13 +1,19 @@
 
-if(typeof document !== "undefined"){
-
-const title = document.querySelectorAll('.ad').forEach(item => {
-    item.addEventListener('click', e => { 
-        let ul = e.currentTarget;
-        let titulo = ul.querySelector('li.title').innerText
-        console.log(titulo)
-       
-    })})
+if (typeof document !== 'undefined') {
+    const adsData = document.querySelectorAll('.ad').forEach(item => {
+        item.addEventListener('click', e => {
+            let ul = e.currentTarget;
+            return {
+                title: ul.querySelector('li.title').innerText,
+                company: ul.querySelector('li.company').innerText,
+                location: ul.querySelector('li.location').innerText,
+                salary: ul.querySelector('li.title').innerText,
+                description: ul.querySelector('li.description').innerText,
+                image: ul.querySelector('li img.imagen').src,
+                link: ul.querySelector('li a.link').href,
+            };
+        });
+    });
 }
 
 
