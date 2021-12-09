ada33c2a44ccd6b48917b438003f1bd3eb9ce301
Ricardo <bescir@gmail.com>
2021-12-09 16:01:18 +0100

    various changes

--- controllers/webControllers.js
+++ controllers/webControllers.js
@@ -11,8 +11,8 @@ const adsWeb = {
         const allAds = await Ad.find({ title: req.body.titleSearched });
         // usar metodo de JS tolowercase en variable
         const scrapingTeAds = await scrapingTe(req.body.titleSearched);
-        // const scrapingTicjobAds = await scrapingTicjob(req.body.titleSearched);
-        const data = [/*scrapingTicjobAds,*/ ...scrapingTeAds, ...allAds]; // Usamos Spread Operator para combinar los arrays
+        const scrapingTicjobAds = await scrapingTicjob(req.body.titleSearched);
+        const data = [...scrapingTicjobAds, ...scrapingTeAds, ...allAds]; // Usamos Spread Operator para combinar los arrays
         res.status(200).render('index', { data: data });
     }),
 };
