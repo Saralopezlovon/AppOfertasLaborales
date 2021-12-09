9959b5c6fee968a79f64ef09db72dc6ab649799b
Sara <lopezlovonsara@gmail.com>
2021-12-10 00:17:38 +0100

    Add style desktop of favorites

--- utils/scrapingTicjob.js
+++ utils/scrapingTicjob.js
@@ -6,7 +6,7 @@ const scrapingTicjob = async tag => {
     try {
         // Abre el navegador (chromium)
         const browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             // devtools: true,
         });
 
